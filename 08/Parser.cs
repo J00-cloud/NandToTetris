307c5a5cba53aa91e518abc6308110a327af79f1
J00-cloud <72215306+J00-cloud@users.noreply.github.com>
2020-10-01 11:53:06 -0400

    Add files via upload

---
+++ 08/Parser.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace VMTranslator
+{
+    class Parser
+    {
+
+        private StreamReader sr;
+
+        public string currentLine;
+        public bool hasMoreCommands = true;
+        public string commandType;
+        public string arg1;
+        public string arg2;
+        public string inAr;
+
+
+        public Parser(string inputFile)
+        {
+            sr = new StreamReader(inputFile);
+        }
+
+
+        public void advance()
+        {
+            currentLine = sr.ReadLine();
+            currentLine = clean(currentLine);
+            hasMoreCommands = !sr.EndOfStream;
+            commandType = getCommandType();
+            getArg1();
+            getArg2();
+
+            if (hasMoreCommands)
+            {    
+            }
+
+            if (currentLine == "push local 0")
+            {
+                Console.WriteLine($" args : {arg1}, {arg2}");
+            }
+
+        }
+        public string clean(string instr)
+        {
+            string outstr = instr.Replace("\t", " ");
+            return outstr;
+        }
+
+        public string getCommandType()
+        {
+            if (currentLine.StartsWith("lt"))
+            {
+                
+                int e =1;
+            }
+            string cdType = currentLine.Split(' ')[0].Trim();
+            inAr = cdType;
+
+            List<string> arithmCds = new List<string>();
+            arithmCds.Add("add");
+            arithmCds.Add("sub");
+            arithmCds.Add("neg");
+            arithmCds.Add("eq");
+            arithmCds.Add("gt");
+            arithmCds.Add("lt");
+            arithmCds.Add("and");
+            arithmCds.Add("or");
+            arithmCds.Add("not");
+
+            if (cdType.Trim() == "push") { return "C_PUSH"; }
+            else if (cdType.Trim() == "pop") { return "C_POP"; }
+            else if ( arithmCds.Contains( cdType.Trim()) ) { return "Arithm"; }
+            else if (cdType.Trim() == "label") { return "C_LABEL"; }
+            else if (cdType.Trim() == "if-goto") { return "C_IFGOTO"; }
+            else if (cdType.Trim() == "goto") { return "C_GOTO"; }
+            else if (cdType.Trim() == "function") { return "C_FUNC"; }
+            else if (cdType.Trim() == "return") { return "C_RETURN"; }
+            else if (cdType.Trim() == "call") { return "C_CALL"; }
+            else
+            {
+                return "null";
+            }
+        }
+
+        public void getArg1()
+        {
+            if(commandType == "C_PUSH" || commandType == "C_POP" || commandType == "C_LABEL" || commandType == "C_IFGOTO" || commandType == "C_GOTO" || commandType == "C_FUNC" || commandType == "C_CALL")
+            {
+                //Console.WriteLine("push");
+                arg1 = currentLine.Split(' ')[1].Trim();
+            }
+
+
+        }
+
+        public void getArg2()
+        {
+            if (commandType == "C_PUSH" || commandType == "C_POP" || commandType == "C_FUNC" || commandType == "C_CALL")
+            {
+                arg2 = currentLine.Split(' ')[2].Trim();
+            }
+        }
+
+        public void close()
+        {
+            sr.Close();
+        }
+
+
+
+
+    }
+}
---
+++ 08/VMTranslator.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace VMTranslator
+{
+    class VMTranslator
+    {
+        static void Main(string[] args)
+        {
+
+            //string file1 = @"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\08\FunctionCalls\NestedCall\Sys.vm";
+            bool folder = true;
+            //string directory = @"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\08\FunctionCalls\StaticsTest\";
+            //string directory = @"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\08\FunctionCalls\FibonacciElement";
+            //string directory = @"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\08\FunctionCalls\NestedCall\Sys.vm";
+            string directory = @args[0];
+
+            if (directory.EndsWith(".asm")|| directory.EndsWith(".vm"))
+            {
+                folder = false;
+            }
+
+            List<string> filesIn = convertFiles(directory);
+
+            //string folderName = (directory).Split("\\")[(directory).Split("\\").Length - 2];
+            string folderName = "";
+            string fileout = "";
+
+            if (!folder)
+            {
+                folderName = Path.GetFileName(Path.GetDirectoryName(directory));
+                string currName = Path.GetFileName(directory);
+                fileout = directory.Replace($"{currName}", $"{folderName}.asm");
+            }
+            else
+            {
+                folderName = Path.GetFileName(Path.GetDirectoryName(directory + Path.DirectorySeparatorChar));
+                fileout = $"{directory}{Path.DirectorySeparatorChar}{folderName}.asm";
+            }
+            //string 
+
+            if (File.Exists(fileout))
+            {
+                File.Delete(fileout);
+            }
+
+            
+            CodeWriter cw = new CodeWriter(fileout);
+
+
+            foreach (string fileIn in filesIn)
+            {
+
+                string fileName = Path.GetFileNameWithoutExtension(fileIn);
+                cw.filename = fileName;
+
+                // get all files in folder
+                // order by sys.vm first
+
+                Parser newFile = new Parser(fileIn);
+
+                if (folder)
+                {
+                    //cw.writePushPop("C_PUSH", "constant", 256);
+                    //cw.writePushPop("C_POP", "SP", -1);
+                    
+                    cw.writeFile("@256");
+                    cw.writeFile("D=A");
+                    cw.writeFile("@SP");
+                    cw.writeFile("M=D");
+                    cw.writeFile("A=D");
+                    cw.functionCall("Sys.init", 0);
+                    folder = false;
+                }
+
+                while (newFile.hasMoreCommands)
+                {
+                    newFile.advance();
+                    if (newFile.commandType != "null")
+                    {
+
+                        if (newFile.commandType == "C_PUSH" || newFile.commandType == "C_POP")
+                        {
+                            //Console.WriteLine("pushOrPop");
+                            cw.writePushPop(newFile.commandType, newFile.arg1, Convert.ToInt32(newFile.arg2));
+                        }
+                        else if (newFile.commandType == "Arithm")
+                        {
+                            //Console.WriteLine("arithm");
+                            //cw.writeArithmetic(newFile.currentLine);
+                            cw.writeArithmetic(newFile.inAr);
+                        }
+                        else if (newFile.commandType == "C_LABEL" || newFile.commandType == "C_IFGOTO" || newFile.commandType == "C_GOTO")
+                        {
+                            //Console.WriteLine("label");
+                            cw.writeFlow(newFile.commandType, newFile.arg1);
+                        }
+                        else if (newFile.commandType == "C_FUNC")
+                        {
+                            cw.functionStart(newFile.arg1, Convert.ToInt32(newFile.arg2));
+
+                        }
+                        else if (newFile.commandType == "C_CALL")
+                        {
+                            cw.functionCall(newFile.arg1, Convert.ToInt32(newFile.arg2));
+                        }
+                        else if (newFile.commandType == "C_RETURN")
+                        {
+                            cw.functionReturn();
+                        }
+
+                    }
+
+
+                }
+                newFile.close();
+
+            }
+
+
+        }
+
+
+
+
+        public static List<string> convertFiles(string inFolder)
+        {
+            // get all the files in a folder with .vm
+            List<string> strList = new List<string>();
+            if (inFolder.EndsWith(".vm"))
+            {
+                strList.Add(inFolder);
+                return strList;
+            }
+
+
+            string[] files = Directory.GetFiles(inFolder, "*.vm");
+            List<string> listOfFiles = new List<string>(files);
+            
+            
+
+            // sort the files
+            if (files.Length == 1)
+            {
+                //File.Delete($"{inFolder}{folderName}.vm");
+                //File.Copy(files[0], $"{inFolder}{folderName}.vm");
+                return listOfFiles;
+            }
+            else
+            {
+                //File.Delete($"{inFolder}{folderName}.vm");
+                // do sys first
+                string toRemove = "";
+                bool rem = false;
+                foreach (var file in listOfFiles)
+                {
+                    if(Path.GetFileName(file) == "Sys.vm")
+                    {
+                        //File.AppendAllLines($"{inFolder}{folderName}.vm", File.ReadAllLines(file));
+                        strList.Add(file);
+                        toRemove = file;
+                        rem = true;
+                    }
+                    
+                }
+                if (rem)
+                {
+                    listOfFiles.Remove(toRemove);
+
+                }
+
+                foreach (var file in listOfFiles)
+                {
+                    strList.Add(file);
+                    //File.AppendAllLines($"{inFolder}{folderName}.vm", File.ReadAllLines(file));
+                }
+            }
+
+            return strList;
+            
+
+        }
+
+
+    }
+}
