d33ae7844db8b151f5db77f67466f4fb2a663d1e
J00-cloud <72215306+J00-cloud@users.noreply.github.com>
2020-10-01 12:03:16 -0400

    Add files via upload

---
+++ 12/instr.bat
@@ -0,0 +1,20 @@
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\12\MathTest"
+
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\12\MemoryTest"
+
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\12\ArrayTest"
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\12\StringTest"
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\12\OutputTest"
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\12\ScreenTest"
+
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\12\KeyboardTest"
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\12\SysTest"
+
+cmd /k
