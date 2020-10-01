b5c561c84d3e3ed524765450c61fd30c1080eb70
J00-cloud <72215306+J00-cloud@users.noreply.github.com>
2020-10-01 11:56:38 -0400

    Add files via upload

---
+++ 09/instr.bat
@@ -0,0 +1,19 @@
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\09\Marius\Main.jack"
+
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\09\Marius\Game.jack"
+
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\09\Marius\Marius.jack"
+
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\09\Marius\Background.jack"
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\09\Marius"
+
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\09\Average"
+
+
+
+"C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\tools\JackCompiler.bat" "C:\Users\jonat\Dropbox\CS\3-Core Systems\From Nand to Tetris\nand2tetris\projects\09\Marius\Square.jack"
